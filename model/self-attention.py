4acc1dae475c11984bf7ab63cc28d06c8e237046
Alessandro Zinni <alessandro.zinni98@icloud.com>
2022-10-19 15:46:08 +0200

    attention module corrected

--- model/self-attention.py
+++ model/self-attention.py
@@ -26,9 +26,11 @@ class SelfAttention(nn.Module):
         self.v = nn.Conv2d(in_channels=in_channels//8,
                            out_channels=in_channels,
                            kernel_size=1)
+        self.softmax = nn.Softmax(dim=1)
         # self.gamma = nn.Parameter(torch.Tensor([0]), requires_grad=True)
 
     def forward(self, x):
+        height, width = x.shape[-2], x.shape[-1]
         f_of_x = self.f(x)
         g_of_x = self.g(x)
         h_of_x = self.h(x)
@@ -42,13 +44,22 @@ class SelfAttention(nn.Module):
         # (b, c~, n)
 
         # Softmax done column-wise (see paper)
-        attention_map = nn.Softmax(einsum("bnc,bcn->bnn", f_of_x, g_of_x),
-                                   dim=1)
+        attention_map = self.softmax(einsum("bij,bjk->bik", f_of_x, g_of_x),)
 
         attention_out_flat = einsum("bcn,bnn->bcn", h_of_x, attention_map)
-        attention_out = self.v(rearrange("b c (h w) -> b c h w", attention_out_flat))
+        attention_out = self.v(rearrange(attention_out_flat,
+                                         "b c (h w) -> b c h w",
+                                         h=height,
+                                         w=width))
         # (b c h w)
 
         out = torch.add(x, attention_out)
 
         return out
+
+
+if __name__ == "__main__":
+    x = torch.randn((2, 16, 8, 8))
+    sa = SelfAttention(16)
+    out = sa(x)
+    print(out.shape)
